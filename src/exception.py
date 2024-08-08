4b6325448084330cee913916c4e1575b6d5e24a5
Shivam Srivastava <shivamkichitthi@gmail.com>
2024-08-09 00:56:07 +0530

    Create the project structre, add Logging and Error Handling

---
+++ src/exception.py
@@ -0,0 +1,22 @@
+import sys
+import logging
+
+
+def error_message_details(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    filename = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+
+    error_message = f"Error occured in Python Script name [{filename}] at line number {line_number} with error message {str(error)}"
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, message, error_detail: sys):
+        super().__init__(message)
+        self.error_message = error_message_details(message, error_detail)
+
+    def __str__(self):
+        return self.error_message
